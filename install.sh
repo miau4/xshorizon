57988ff1a3d72ccb402ac029d33a0af281292de9
Supremo <103901456+miau4@users.noreply.github.com>
2026-03-16 03:52:51 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,33 @@
+#!/bin/bash
 
+clear
+
+echo "================================"
+echo "   INSTALADOR XSHORIZON"
+echo "================================"
+
+sleep 2
+
+apt update -y
+apt install curl wget jq unzip uuid-runtime -y
+
+mkdir -p /etc/xray
+mkdir -p /etc/xshorizon
+
+echo "Instalando Xray..."
+
+bash -c "$(curl -L https://raw.githubusercontent.com/XTLS/Xray-install/main/install-release.sh)" @ install
+
+echo "Baixando painel..."
+
+wget -O /usr/local/bin/xshorizon https://raw.githubusercontent.com/miau4/xshorizon/main/xshorizon.sh
+
+chmod +x /usr/local/bin/xshorizon
+
+echo ""
+echo "Instalação concluída"
+echo ""
+
+sleep 2
+
+xshorizon
